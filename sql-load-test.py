b43c6cd31ab9665ad1f4c79942ab69f6a6aa3dec
prabhapa <116177314+prabhapa@users.noreply.github.com>
2024-07-09 11:53:57 +0530

    Update sql-load-test.py

--- sql-load-test.py
+++ sql-load-test.py
@@ -2,5 +2,5 @@
 
 def get_query():
     return """
-SELECT ID, Name, Age, Salary, Department, City, Country FROM loadtesttable WHERE Name like 'A%' AND Age BETWEEN 30 AND 40
+SELECT ID, Name, Age, Salary, Department, City, Country FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY Department ORDER BY Salary DESC) AS R FROM loadtesttable) AS ranked WHERE R <= 5
 """
